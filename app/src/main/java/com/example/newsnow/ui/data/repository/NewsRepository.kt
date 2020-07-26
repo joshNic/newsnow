eccda3aeb3ee4120491c22e0a7f6aab9b57ff3a6
joshNic <256jomu@gmail.com>
2020-07-27 00:45:15 +0300

    add repository

---
+++ app/src/main/java/com/example/newsnow/ui/data/repository/NewsRepository.kt
@@ -0,0 +1,21 @@
+package com.example.newsnow.ui.data.repository
+
+import com.example.newsnow.ui.data.api.RetrofitInstance
+import com.example.newsnow.ui.data.db.ArticleDatabase
+import com.example.newsnow.ui.data.entity.Article
+
+class NewsRepository(
+    val db: ArticleDatabase
+) {
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
+
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
+        RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
+
+    fun getSavedNews() = db.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticle(article: Article) = db.getArticleDao().deleteArticle(article)
+}
