42087462125d869973cbe5634b91c868ad2c151e
joshNic <256jomu@gmail.com>
2020-07-27 14:08:40 +0300

    add single article display functionality with webview

--- app/src/main/java/com/example/newsnow/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/newsnow/ui/fragments/ArticleFragment.kt
@@ -1,32 +1,30 @@
 package com.example.newsnow.ui.fragments
 
-import androidx.lifecycle.ViewModelProviders
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+
 import android.view.View
-import android.view.ViewGroup
+
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.navArgs
 import com.example.newsnow.R
+import com.example.newsnow.ui.NewsActivity
+import com.example.newsnow.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_article.*
 
-class ArticleFragment : Fragment() {
+class ArticleFragment : Fragment(R.layout.fragment_article) {
 
-    companion object {
-        fun newInstance() = ArticleFragment()
+    lateinit var viewModel: NewsViewModel
+    val args: ArticleFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+        val article = args.article
+        webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(article.url)
+        }
     }
-
-    private lateinit var viewModel: ArticleViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_article, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ArticleViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
