efa30b08d996b3f700720362c3cc037a9e91c295
joshNic <256jomu@gmail.com>
2020-07-27 13:13:02 +0300

    add search news

--- app/src/main/java/com/example/newsnow/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/newsnow/ui/NewsViewModel.kt
@@ -13,6 +13,9 @@ class NewsViewModel(val newsRepository:NewsRepository): ViewModel() {
     val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     var breakingNewsPage=1
 
+    val searchNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var searchNewsPage = 1
+
     init {
         getBreakingNews("us")
     }
@@ -25,6 +28,19 @@ class NewsViewModel(val newsRepository:NewsRepository): ViewModel() {
     private fun handleBreakingNewsResponse(response:Response<NewsResponse>):Resource<NewsResponse>{
         if(response.isSuccessful){
             response.body()?.let { resultResponse->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+    fun searchNews(searchQuery: String) = viewModelScope.launch {
+        searchNews.postValue(Resource.Loading())
+        val response = newsRepository.searchNews(searchQuery, searchNewsPage)
+        searchNews.postValue(handleSearchNewsResponse(response))
+    }
+    private fun handleSearchNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse> {
+        if(response.isSuccessful) {
+            response.body()?.let { resultResponse ->
                 return Resource.Success(resultResponse)
             }
         }
--- app/src/main/java/com/example/newsnow/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/example/newsnow/ui/fragments/SearchNewsFragment.kt
@@ -1,32 +1,85 @@
 package com.example.newsnow.ui.fragments
 
-import androidx.lifecycle.ViewModelProviders
+
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+
 import android.view.View
-import android.view.ViewGroup
+
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsnow.R
+import com.example.newsnow.data.adapters.NewsAdapter
+import com.example.newsnow.ui.NewsActivity
+import com.example.newsnow.ui.NewsViewModel
+import com.example.newsnow.util.Constants.Companion.SEARCH_NEWS_TIME_DELAY
+import com.example.newsnow.util.Resource
+import kotlinx.android.synthetic.main.fragment_search_news.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class SearchNewsFragment : Fragment() {
+class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
 
-    companion object {
-        fun newInstance() = SearchNewsFragment()
+    lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    val TAG = "SearchNewsFragment"
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        var job: Job? = null
+        etSearch.addTextChangedListener { editable ->
+            job?.cancel()
+            job = MainScope().launch {
+                delay(SEARCH_NEWS_TIME_DELAY)
+                editable?.let {
+                    if (editable.toString().isNotEmpty()) {
+                        viewModel.searchNews(editable.toString())
+                    }
+                }
+            }
+        }
+
+        viewModel.searchNews.observe(viewLifecycleOwner, Observer { response ->
+            when (response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error occured: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
     }
 
-    private lateinit var viewModel: SearchNewsViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_search_news, container, false)
+    private fun hideProgressBar() {
+        paginationProgressBar.visibility = View.INVISIBLE
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(SearchNewsViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun showProgressBar() {
+        paginationProgressBar.visibility = View.VISIBLE
     }
 
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvSearchNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
 }
