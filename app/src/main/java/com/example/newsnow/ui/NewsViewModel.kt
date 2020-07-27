86f369bcac7d7018fd29b183d25c2c348d38bc39
joshNic <256jomu@gmail.com>
2020-07-27 14:50:12 +0300

    add save article to database

--- app/src/main/java/com/example/newsnow/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/newsnow/ui/NewsViewModel.kt
@@ -3,6 +3,7 @@ package com.example.newsnow.ui
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.newsnow.data.entity.Article
 import com.example.newsnow.data.entity.NewsResponse
 import com.example.newsnow.data.repository.NewsRepository
 import com.example.newsnow.util.Resource
@@ -45,5 +46,14 @@ class NewsViewModel(val newsRepository:NewsRepository): ViewModel() {
             }
         }
         return Resource.Error(response.message())
+    }
+    fun saveArticle(article: Article) = viewModelScope.launch {
+        newsRepository.upsert(article)
+    }
+
+    fun getSavedNews() = newsRepository.getSavedNews()
+
+    fun deleteArticle(article: Article) = viewModelScope.launch {
+        newsRepository.deleteArticle(article)
     }
 }
--- app/src/main/java/com/example/newsnow/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/newsnow/ui/fragments/ArticleFragment.kt
@@ -11,6 +11,7 @@ import androidx.navigation.fragment.navArgs
 import com.example.newsnow.R
 import com.example.newsnow.ui.NewsActivity
 import com.example.newsnow.ui.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_article.*
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
@@ -25,6 +26,11 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
         webView.apply {
             webViewClient = WebViewClient()
             loadUrl(article.url)
+        }
+
+        fab.setOnClickListener{
+            viewModel.saveArticle(article)
+            Snackbar.make(view,"Article saved successfully",Snackbar.LENGTH_SHORT).show()
         }
     }
 }
