6ccbc3ed5b94b767b7a615244ed5f79a87690590
joshNic <256jomu@gmail.com>
2020-07-27 14:59:58 +0300

    implement saved articles fragment

--- app/src/main/java/com/example/newsnow/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/example/newsnow/ui/fragments/SavedNewsFragment.kt
@@ -1,32 +1,45 @@
 package com.example.newsnow.ui.fragments
 
-import androidx.lifecycle.ViewModelProviders
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+
 import android.view.View
-import android.view.ViewGroup
+
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsnow.R
+import com.example.newsnow.data.adapters.NewsAdapter
+import com.example.newsnow.ui.NewsActivity
+import com.example.newsnow.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_saved_news.*
 
-class SavedNewsFragment : Fragment() {
+class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
 
-    companion object {
-        fun newInstance() = SavedNewsFragment()
+    lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(
+                R.id.action_savedNewsFragment_to_articleFragment,
+                bundle
+            )
+        }
     }
 
-    private lateinit var viewModel: SavedNewsViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_saved_news, container, false)
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvSavedNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(SavedNewsViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
