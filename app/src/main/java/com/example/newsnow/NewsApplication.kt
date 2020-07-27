e238b34c1d36ea62eb147649171ed097841651f3
joshNic <256jomu@gmail.com>
2020-07-27 16:23:27 +0300

    add internet exception handling logic

---
+++ app/src/main/java/com/example/newsnow/NewsApplication.kt
@@ -0,0 +1,5 @@
+package com.example.newsnow
+
+import android.app.Application
+
+class NewsApplication : Application()
--- app/src/main/java/com/example/newsnow/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/newsnow/ui/NewsViewModel.kt
@@ -1,8 +1,16 @@
 package com.example.newsnow.ui
 
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.ConnectivityManager.*
+import android.net.NetworkCapabilities.*
+import android.os.Build
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.newsnow.NewsApplication
 import com.example.newsnow.data.entity.Article
 import com.example.newsnow.data.entity.NewsResponse
 import com.example.newsnow.data.repository.NewsRepository
@@ -11,8 +19,9 @@ import kotlinx.coroutines.launch
 import retrofit2.Response
 
 class NewsViewModel(
+    app: Application,
     val newsRepository: NewsRepository
-) : ViewModel() {
+) : AndroidViewModel(app) {
 
     val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     var breakingNewsPage = 1
@@ -81,5 +90,31 @@ class NewsViewModel(
 
     fun deleteArticle(article: Article) = viewModelScope.launch {
         newsRepository.deleteArticle(article)
+    }
+
+    private fun hasInternetConnection(): Boolean {
+        val connectivityManager = getApplication<NewsApplication>().getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val activeNetwork = connectivityManager.activeNetwork ?: return false
+            val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+            return when {
+                capabilities.hasTransport(TRANSPORT_WIFI) -> true
+                capabilities.hasTransport(TRANSPORT_CELLULAR) -> true
+                capabilities.hasTransport(TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.activeNetworkInfo?.run {
+                return when(type) {
+                    TYPE_WIFI -> true
+                    TYPE_MOBILE -> true
+                    TYPE_ETHERNET -> true
+                    else -> false
+                }
+            }
+        }
+        return false
     }
 }
