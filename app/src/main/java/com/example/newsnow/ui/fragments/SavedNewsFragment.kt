c03d9f1bd41a71772ae2d9e969f593eb3d029478
joshNic <256jomu@gmail.com>
2020-07-27 15:00:57 +0300

    add get articles from database

--- app/src/main/java/com/example/newsnow/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/example/newsnow/ui/fragments/SavedNewsFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 import android.view.View
+import androidx.lifecycle.Observer
 
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -33,6 +34,9 @@ class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
                 bundle
             )
         }
+        viewModel.getSavedNews().observe(viewLifecycleOwner, Observer { articles ->
+            newsAdapter.differ.submitList(articles)
+        })
     }
 
     private fun setupRecyclerView() {
